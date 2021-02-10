f76a1a75e1ea28ac2e72674085aa1ebcd325d80f
vkp222 <65462395+vkp222@users.noreply.github.com>
2021-02-10 18:42:25 +0530

    Add files via upload

---
+++ type.js
@@ -0,0 +1,17 @@
+function register() {
+    const x = document.getElementById("Log-in");
+    const y = document.getElementById("register");
+    const z = document.getElementById("btn");
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "110px";
+}
+
+function login() {
+    const x = document.getElementById("Log-in");
+    const y = document.getElementById("register");
+    const z = document.getElementById("btn");
+    x.style.left = "50px";
+    y.style.left = "450px";
+    z.style.left = "0px";
+}
